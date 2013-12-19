f17c4870ffdfbaa2c3a22f3d65127cbb25630b45
Hugo Wood <hx4.5@free.fr>
2013-12-20 00:49:23 +0100

    use constants instead of magic numbers for HTTP statuses

--- src/main/java/fr/notfound/rest/JavaNetUrlRestGetClient.java
+++ src/main/java/fr/notfound/rest/JavaNetUrlRestGetClient.java
@@ -1,12 +1,11 @@
 package fr.notfound.rest;
 
 import static java.lang.String.format;
+import static java.net.HttpURLConnection.HTTP_OK;
 
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
-
-
 
 import com.google.common.base.Charsets;
 import com.google.common.io.CharStreams;
@@ -18,7 +17,7 @@ public class JavaNetUrlRestGetClient implements RestGetClient {
             HttpURLConnection connection = (HttpURLConnection)url.openConnection();
             connection.setRequestMethod("GET");
             connection.connect();
-            if (connection.getResponseCode() != 200)
+            if (connection.getResponseCode() != HTTP_OK)
                 throw new RuntimeException(format("HTTP response code of %s was %s", url, connection.getResponseCode()));
             try (Reader reader = new BufferedReader(
                     new InputStreamReader(connection.getInputStream(), Charsets.UTF_8))) {
