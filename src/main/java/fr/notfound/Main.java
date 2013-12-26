c00a64c65d1e73b4d1cad7bc893a5d6753c5823a
Hugo Wood <hx4.5@free.fr>
2013-12-26 23:42:21 +0100

    URLs stored in a properties file as Velocity templates

--- src/main/java/fr/notfound/Main.java
+++ src/main/java/fr/notfound/Main.java
@@ -2,6 +2,14 @@ package fr.notfound;
 
 import static java.lang.Integer.parseInt;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import org.apache.velocity.app.VelocityEngine;
+
+import com.google.common.collect.ImmutableMap;
+
 public class Main {
 
     private static final int argArenaUrl = 0;
@@ -12,10 +20,26 @@ public class Main {
     private static Jetty server;
 
     public static void main(String[] args) {
+        Properties urls = urls();
+        
+        TemplateRenderer templateRenderer = new TemplateRenderer(new VelocityEngine());
+        String teamIdUrl = templateRenderer.render(urls.getProperty("teamId"), ImmutableMap.of(
+            "teamName", args[argTeamName], 
+            "password", args[argPassword]));
+        
         RestClient client = new RestClient();
-        String teamId = client.get(
-            args[argArenaUrl] + "/player/getIdEquipe/" + args[argTeamName] + "/" + args[argPassword]);
+        String teamId = client.get(args[argArenaUrl] + "/" + teamIdUrl);
         server = Jetty.onPort(parseInt(args[argMonitoringPort])).handle("/", teamId).start();
+    }
+    
+    private static Properties urls() {
+        Properties urls = new Properties();
+        try (InputStream inputStream = Main.class.getClassLoader().getResourceAsStream("urls.properties")) {
+            urls.load(inputStream);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return urls;
     }
 
     public static void stop() {
