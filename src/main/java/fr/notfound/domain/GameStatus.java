06187ce93ce2cec7995fdf64a252db2eed67b300
Hugo Wood <hx4.5@free.fr>
2013-12-31 19:20:21 +0100

    Implemented GameStatus and MoveResult values (see issue #1)

--- src/main/java/fr/notfound/domain/GameStatus.java
+++ src/main/java/fr/notfound/domain/GameStatus.java
@@ -1,11 +1,21 @@
 package fr.notfound.domain;
 
 public enum GameStatus {
-    ;
+    YourTurn,
+    NotYourTurn,
+    Won,
+    Lost,
+    Canceled;
 
     public static GameStatus parse(String status) {
-        // TODO Auto-generated method stub
-        return null;
+        switch (status) {
+            case "OUI": return YourTurn;
+            case "NON": return NotYourTurn;
+            case "GAGNE": return Won;
+            case "PERDU": return Lost;
+            case "ANNULE": return Canceled;
+            default: throw new IllegalArgumentException("illegal game status: " + status);
+        }
     }
 
 }
--- src/main/java/fr/notfound/domain/MoveResult.java
+++ src/main/java/fr/notfound/domain/MoveResult.java
@@ -1,11 +1,19 @@
 package fr.notfound.domain;
 
 public enum MoveResult {
-    ;
+    Approved,
+    Rejected,
+    Victory,
+    NotYourTurn;
 
-    public static MoveResult parse(String play) {
-        // TODO Auto-generated method stub
-        return null;
+    public static MoveResult parse(String moveResult) {
+        switch (moveResult) {
+            case "OK": return Approved;
+            case "KO": return Rejected;
+            case "GAGNE": return Victory;
+            case "PTT": return NotYourTurn;
+            default: throw new IllegalArgumentException("illegal move result: " + moveResult);
+        }
     }
 
 }
