2ed7365c2a5be2fe18583673b38dc546646a6cb9
Hugo Wood <hx4.5@free.fr>
2014-01-06 23:16:56 +0100

    Board gets a real implementation to enable testing

--- src/main/java/fr/notfound/domain/Board.java
+++ src/main/java/fr/notfound/domain/Board.java
@@ -4,9 +4,19 @@ import fr.notfound.meta.ValueType;
 
 @ValueType
 public class Board {
+    
+    private final String representation;
+
+    public Board(String board) {
+        this.representation = board;
+    }
+    
+    @Override public String toString() {
+        return "Board " + representation;
+    }
 
     public static Board parse(String board) {
-        throw new UnsupportedOperationException("TODO: implement on battle-day");
+        return new Board(board);
     }
 
 }
