d6b2fd8897360a05b11e4a8f8750f675bc531782
Hugo Wood <hx4.5@free.fr>
2014-01-06 23:16:37 +0100

    added custom Move.toString

--- src/main/java/fr/notfound/domain/Move.java
+++ src/main/java/fr/notfound/domain/Move.java
@@ -1,5 +1,6 @@
 package fr.notfound.domain;
 
+import static java.lang.String.format;
 import fr.notfound.meta.ValueType;
 
 @ValueType
@@ -11,6 +12,10 @@ public class Move {
     public Move(String x, String y) {
         this.x = x;
         this.y = y;
+    }
+    
+    @Override public String toString() {
+        return format("{Move: %s, %s}", x, y);
     }
 
     public static Move parse(String coordinates) {
