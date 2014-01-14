9cc218f768cb1fbed177ea3b2ed3922d499fd915
Hugo Wood <hugo.wood@sopragroup.com>
2014-01-14 19:02:17 +0100

    correct battle arena uri

--- src/main/java/fr/notfound/BattleDayParameters.java
+++ src/main/java/fr/notfound/BattleDayParameters.java
@@ -8,7 +8,7 @@ public class BattleDayParameters {
         "http://ec2-54-200-12-98.us-west-2.compute.amazonaws.com/csnbattlearena/webservices/test/");
     
     // FIXME: Enable and correct on battle day
-    public static final URI ArenaUri = URI.create("http://192.168.2.2:8080/csnbattlearena/webservices");
+    public static final URI ArenaUri = URI.create("http://192.168.2.2:8080/csnbattlearena/webservices/battle");
     public static final String TeamName = "404";
     public static final String Password = "";
     public static final int NumberOfOpponentTeams = 4;
