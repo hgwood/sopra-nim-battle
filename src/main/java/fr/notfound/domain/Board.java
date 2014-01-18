369ac1957cbca16134c22a2c43827ab730c44216
Hugo Wood <hx4.5@free.fr>
2014-01-18 13:31:28 +0100

    add groups() method on Board : returns groups of sticks with position and size

--- src/main/java/fr/notfound/domain/Board.java
+++ src/main/java/fr/notfound/domain/Board.java
@@ -1,8 +1,13 @@
 package fr.notfound.domain;
 
+import static com.google.common.collect.Lists.newArrayList;
 import static java.lang.String.format;
 
+import java.util.List;
 import java.util.Objects;
+import java.util.regex.Pattern;
+
+import com.google.common.base.Splitter;
 
 import fr.notfound.meta.ValueType;
 
@@ -21,6 +26,40 @@ public class Board {
     
     public int firstAvailable() {
         return representation.indexOf("1");
+    }
+    
+    public int length() {
+        return representation.length();
+    }
+    
+    public List<Move> groups() {
+        //Iterable<String> groupsOfOnes = Splitter.on("0").omitEmptyStrings().split(representation);
+        List<Move> groups = newArrayList();
+        int currentGroupIndex = 0;
+        int currentGroupSize = 0;
+        boolean capturing = false;
+        for (int i = 0; i < representation.length(); i++) {
+            if (representation.charAt(i) == '1') {
+                if (capturing) {
+                    currentGroupSize += 1;
+                } else {
+                    capturing = true;
+                    currentGroupIndex = i;
+                    currentGroupSize = 1;
+                }
+                if (i == representation.length() - 1) {
+                    groups.add(new Move(currentGroupIndex, currentGroupSize));
+                }
+            } else {
+                if (capturing) {
+                    capturing = false;
+                    groups.add(new Move(currentGroupIndex, currentGroupSize));
+                }
+            }
+            
+        }
+        
+        return groups;
     }
     
     @Override public boolean equals(Object obj) {
